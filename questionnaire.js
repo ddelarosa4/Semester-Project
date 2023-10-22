5823cef36a3c3613a8c847ad996a0635de0121ff
setiennes <138072035+setiennes@users.noreply.github.com>
2023-10-22 15:54:25 -0500

    Update questionnaire.js

--- questionnaire.js
+++ questionnaire.js
@@ -1,14 +1,5 @@
 function determine next question (answer){
-switch(answer) {
-  case 1:
-    response;
-    break;
-  case 2:
-    response;
-    break;
-  default:
-    response;
-    break;
-  }
-  return response
+ // TODO Replace with actual algorithm
+
+   //return [resulting subcategory url];
 }
