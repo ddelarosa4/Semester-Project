acb545cefb2a69da34594b1033cf566ae59d50bc
setiennes <138072035+setiennes@users.noreply.github.com>
2023-10-22 13:20:25 -0500

    Create questionnaire.js

---
+++ questionnaire.js
@@ -0,0 +1,14 @@
+function determine next question (answer){
+switch(answer) {
+  case 1:
+    response;
+    break;
+  case 2:
+    response;
+    break;
+  default:
+    response;
+    break;
+  }
+  return response
+}
