595bfd63a66a8cc40bee0685a48ebc43270e04f9
ddelarosa4 <ddelarosa4@twu.edu>
2023-11-13 22:18:36 -0600

    Nav Bar working just needs color

---
+++ Subjetcs/nav.js
@@ -0,0 +1,23 @@
+
+document.write(`
+<head>
+    <link rel="stylesheet" type="text/css" href="../styles.css"></link>
+</head>
+<nav>
+    <ul>
+        <li class="dropdown" onmouseover="toggleDropdown(true)" onmouseout="toggleDropdown(false)">
+            <a href="#">Subjects</a>
+            <div class="dropdown-box" id="services-box">
+                <!-- Content for the dropdown box -->
+                <a href="math.html">Math</a>
+                <a href="science.html">Science</a>
+                <a href="computerScience.html">Computer Science</a>
+                <a href="history.html">History </a>
+                <a href="english.html">English </a>
+            </div>
+        </li>
+        <li><a href="../resources.html">General Resources</a></li>
+    </ul>
+</nav>
+<script src="script.js"></script>
+`);
---
+++ nav.js
@@ -0,0 +1,24 @@
+
+document.write(`
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css"></link>
+</head>
+<nav>
+    <ul>
+        <li class="dropdown" onmouseover="toggleDropdown(true)" onmouseout="toggleDropdown(false)">
+            <a href="#">Subjects</a>
+            <div class="dropdown-box" id="services-box">
+                <!-- Content for the dropdown box -->
+                
+                    <a href="Subjetcs/math.html">Math</a>
+                    <a href="Subjetcs/science.html">Science</a>
+                   <a href="Subjetcs/computerScience.html">Computer Science</a>
+                   <a href="Subjetcs/history.html">History </a>
+                <a href="Subjetcs/english.html">English </a>
+            </div>
+        </li>
+        <li><a href="resources.html">General Resources</a></li>
+    </ul>
+</nav>
+<script src="script.js"></script>
+`);
