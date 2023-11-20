d4a0ea467c854c85fb3e13f81718048e628b9117
ddelarosa4 <ddelarosa4@twu.edu>
2023-11-19 22:26:12 -0600

    Added more to the nav

--- Subjetcs/nav.js
+++ Subjetcs/nav.js
@@ -1,24 +1,39 @@
+// ... Your existing code ...
 
-document.write(`
-<head>
-    <link rel="stylesheet" type="text/css" href="../styles.css"></link>
-</head>
-<nav>
+function toggleDropdown() {
+    var box = document.querySelector("#services-box");
+    var arrow = document.querySelector(".arrow");
+
+    box.classList.toggle('show');
+    arrow.classList.toggle('up', box.classList.contains('show'));
+}
+
+
+
+
+function toggleNav() {
+    var navList = document.querySelector('nav ul');
+    navList.classList.toggle('show');
+}
+
+const navContainer = document.createElement('nav');
+navContainer.innerHTML = `
+    <div class="nav-icon" onclick="toggleNav()">&#9776;</div>
     <ul>
-        <li> <a href="../index.html">Home</a><li>
-        <li class="dropdown" onmouseover="toggleDropdown(true)" onmouseout="toggleDropdown(false)">
-            <a href="#">Subjects</a>
+        <li><a href="../index.html">Home</a></li>
+        <li class="dropdown" onclick="toggleDropdown()">
+            <a href="#"><span class="arrow">&#9660;</span>Subjects
             <div class="dropdown-box" id="services-box">
                 <!-- Content for the dropdown box -->
                 <a href="Math.html">Math</a>
                 <a href="science.html">Science</a>
                 <a href="computerScience.html">Computer Science</a>
-                <a href="history.html">History </a>
                 <a href="english.html">English </a>
             </div>
+            
         </li>
-        <li><a href="../resources.html">General Resources</a></li>
     </ul>
-</nav>
-<script src="script.js"></script>
-`);
+`;
+
+document.body.appendChild(navContainer);
+
